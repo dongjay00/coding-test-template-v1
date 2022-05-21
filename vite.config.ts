290233511b76cb944bc510c8ddb557013950cfe4
dongjay00 <cjswodlehdgn@gmail.com>
2022-05-22 00:43:33 +0900

    [setting]: eslint, prettier setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     legacy({
       targets: ["defaults", "not IE 11"],
     }),
