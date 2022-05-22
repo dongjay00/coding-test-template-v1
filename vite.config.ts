ab9af27ea8bf5ec5cdc5f397de6781605b90ca26
dongjay00 <cjswodlehdgn@gmail.com>
2022-05-22 10:20:40 +0900

    [setting]: eslint setting 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import legacy from "@vitejs/plugin-legacy";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+/* eslint-disable import/no-extraneous-dependencies */
+import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ["@emotion/babel-plugin"],
+        plugins: ['@emotion/babel-plugin'],
       },
     }),
     tsconfigPaths(),
     legacy({
-      targets: ["defaults", "not IE 11"],
+      targets: ['defaults', 'not IE 11'],
     }),
   ],
   esbuild: {
@@ -22,9 +23,11 @@ export default defineConfig({
     jsxInject: `import { jsx } from '@emotion/react'`,
   },
   resolve: {
-    alias: [{
-      find: '@',
-      replacement: '/src'
-    }]
-  }
+    alias: [
+      {
+        find: '@',
+        replacement: '/src',
+      },
+    ],
+  },
 });
