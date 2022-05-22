5107562891ea4f7420c0e122b5c6c87955042e55
dongjay00 <cjswodlehdgn@gmail.com>
2022-05-22 10:00:59 +0900

    [setting]: install recoil, react-query, emotion

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
     tsconfigPaths(),
     legacy({
       targets: ["defaults", "not IE 11"],
     }),
   ],
+  esbuild: {
+    jsxFactory: `jsx`,
+    jsxInject: `import { jsx } from '@emotion/react'`,
+  },
   resolve: {
     alias: [{
       find: '@',
